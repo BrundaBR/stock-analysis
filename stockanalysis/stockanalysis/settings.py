c3d67be754e0edf159e1b92fb0e5e76cd1ff8bc3
BrundaBR <revadibrunda@gmail.com>
2020-12-18 16:33:21 +0530

    design complete

--- stockanalysis/stockanalysis/settings.py
+++ stockanalysis/stockanalysis/settings.py
@@ -22,7 +22,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 
+SECRET_KEY = 'q_+!8el2kjb_%gc3gwz2_bu7f@jo_iv3$z087e=*%1s(kfkka+'
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
